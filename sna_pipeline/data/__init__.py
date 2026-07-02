3ce2a342e4f1784f3c1108097e48f7c378e9e31a
YoranB <90249440+YoranB@users.noreply.github.com>
2026-07-02 13:28:09 +0200

    Add pandas compatibility for series fallback replace

--- sna_pipeline/data/__init__.py
+++ sna_pipeline/data/__init__.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+_ORIGINAL_SERIES_REPLACE = pd.Series.replace
+
+
+def _series_replace_compat(self, to_replace=None, value=None, *args, **kwargs):
+    if to_replace == "" and isinstance(value, pd.Series):
+        return self.where(self != "", value)
+    return _ORIGINAL_SERIES_REPLACE(self, to_replace=to_replace, value=value, *args, **kwargs)
+
+
+pd.Series.replace = _series_replace_compat
